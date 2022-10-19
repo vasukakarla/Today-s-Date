86d3baf059e6e9d2ac17925df7011a59c632bf06
vasundhara <vasukakarla89@gmail.com>
2022-10-19 12:17:58 +0000

    Today's Date

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+  response.send(`${day}-${month + 1}-${year}`);
+});
+module.exports = app;
